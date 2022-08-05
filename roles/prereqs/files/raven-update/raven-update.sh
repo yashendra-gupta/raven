57c414f04e53abe9939dcf5896ec6aad483aaf84
Yashendra Gupta <40363062+yashendra-gupta@users.noreply.github.com>
2022-08-05 15:39:06 +0530

    Update raven-update.sh

--- roles/prereqs/files/raven-update/raven-update.sh
+++ roles/prereqs/files/raven-update/raven-update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "hello from raven-update.sh"
+
+ansible-pull --accept-host-key --private-key="/home/vagrant/.ssh/id_rsa"  --url="git@github.com:yashendra-gupta/raven.git" local.yml
