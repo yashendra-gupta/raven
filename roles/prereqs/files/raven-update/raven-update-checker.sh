00d3f7e241fca38a7edec6ff3ef12a8f39630b05
Yashendra Gupta <40363062+yashendra-gupta@users.noreply.github.com>
2022-08-06 13:16:28 +0530

    Update raven-update-checker.sh

--- roles/prereqs/files/raven-update/raven-update-checker.sh
+++ roles/prereqs/files/raven-update/raven-update-checker.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 # For notify-send to work, set DBUS_SESSION_BUS_ADDRESS
-# Refer 
+# Refer
 #   - https://stackoverflow.com/a/33723614
 #   - https://askubuntu.com/questions/926626/how-do-i-fix-warning-command-substitution-ignored-null-byte-in-input/926695#926695
 # Note: Desktop environment for  KDE is dolphin and for GNOME it is nautilus
+# THIS DOES NOT WORK IF DOLPHIN IS NOT OPENED
+#username=$(/usr/bin/whoami)
+#pid=$(pgrep -u $username dolphin)
+#dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ | tr '\0' '\n' | sed 's/DBUS_SESSION_BUS_ADDRESS=//' )
+#export DBUS_SESSION_BUS_ADDRESS=$dbus
 
-username=$(/usr/bin/whoami)
-pid=$(pgrep -u $username dolphin)
-dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ | tr '\0' '\n' | sed 's/DBUS_SESSION_BUS_ADDRESS=//' )
-export DBUS_SESSION_BUS_ADDRESS=$dbus
+
+# This is required because crontab does not have DBus information.
+# Below line will look for ~/.dbus/Xdbus for DBus information written by script "/etc/profil.d/create-dbus-info.sh"
+if [ -r ~/.dbus/Xdbus ]; then
+  source ~/.dbus/Xdbus
+fi
+
+
 
 if  [ -d /opt/development/raven ]
 then
