885106fab5c95fc3b1874f42cea55bc0f590e09d
Yashendra Gupta <40363062+yashendra-gupta@users.noreply.github.com>
2022-09-10 15:19:23 +0530

    use raven cow for raven-update

--- roles/prereqs/files/raven-update/raven-update.sh
+++ roles/prereqs/files/raven-update/raven-update.sh
@@ -2,7 +2,7 @@
 
 echo "hello from raven-update.sh"
 
-sudo -E ansible-pull --accept-host-key --private-key="/home/vagrant/.ssh/id_rsa"  --url="git@github.com:yashendra-gupta/raven.git" local.yml
+sudo -E ANSIBLE_COW_SELECTION=raven ansible-pull --accept-host-key --private-key="/home/vagrant/.ssh/id_rsa"  --url="git@github.com:yashendra-gupta/raven.git" local.yml
 
 currentDirectory=$(pwd)
 
