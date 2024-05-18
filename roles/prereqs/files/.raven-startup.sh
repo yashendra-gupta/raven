50b4685154224b8a5545436338995ea69e61f357
Yashendra Gupta <yashendragupta2704@gmail.com>
2024-05-18 09:33:16 +0530

    0.0.349 Changes: 1) created directories for new structure for Raven  2) Added version.txt

--- roles/prereqs/files/.raven-startup.sh
+++ roles/prereqs/files/.raven-startup.sh
@@ -3,3 +3,6 @@
 # wait for 5 minutes and kill processes related to software update pop up
 sleep 300
 killall -15 DiscoverNotifier pamac-tray-plasma msm_kde_notifier
+
+# copy rave version file
+cp /opt/development/raven/version.txt $RAVEN_HOME/version.txt
---
+++ roles/prereqs/files/bashrc/raven-directories.bashrc
@@ -0,0 +1,6 @@
+export RAVEN_HOME=/opt/raven
+export RAVEN_SRC_HOME=$RAVEN_HOME/.raven
+export RAVEN_WALLPAPER=$RAVEN_HOME/wallpaper
+export RAVEN_BIN=$RAVEN_HOME/bin
+export RAVEN_LOG=$RAVEN_HOME/log
+export RAVEN_PKG=$RAVEN_HOME/package
